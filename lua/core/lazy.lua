bf9df48d1e41305c7811a6504010832c31d2c1b2
Jeremi Klinke <jklinke@cinemo.com>
2024-05-10 11:13:27 +0200

    Add copilot and submodules

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -10,7 +10,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({ 
+require("lazy").setup({
         {import = "core.plugins"},
     	{import = "core.plugins.lsp"},
         'tpope/vim-fugitive',
