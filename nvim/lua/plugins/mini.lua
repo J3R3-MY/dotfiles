d7d01c48ea2d33ff7e9aec030c0eeeee55185870
Jeremi <urnmd@student.kit.edu>
2025-04-16 11:17:13 +0200

    Add arrow.lua and remove mini.statusline config
    -Added arrow.lua for easier jumping between files
    -Removed unused mini.statusline config

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -7,44 +7,6 @@ return {
     require('mini.pairs').setup {}
     require('mini.comment').setup {}
     require('mini.icons').setup {}
-    -- require('mini.statusline').setup({
-    --   content = {
-    --     active = function()
-    --       local MiniStatusline = require("mini.statusline")
-    --
-    -- 	local function shorten_string(str, max_length)
-    -- 		if #str > max_length then
-    -- 			return string.sub(str, 1, max_length) .. "..."
-    -- 		else
-    -- 			return str
-    -- 		end
-    -- 	end
-    --
-    --       -- Sections from the first configuration
-    --       local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-    --       local git_og = MiniStatusline.section_git({ trunc_width = 40 })
-    -- 	local diff = MiniStatusline.section_diff({ trunc_width = 75 })
-    --       local filename = MiniStatusline.section_filename({ trunc_width = 100 })
-    -- 	local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-    --       local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-    --
-    -- 	local git = shorten_string(git_og, 25)
-    --
-    --       return MiniStatusline.combine_groups({
-    --         { hl = mode_hl, strings = { mode:upper() } },
-    --         { hl = "MiniStatuslineDevinfo", strings = { git, diagnostics, diff } },
-    --         "%<", -- Mark general truncate point
-    --         { hl = "MiniStatuslineFilename", strings = { filename} },
-    --         "%=", -- End left alignment
-    --         {
-    --           hl = "MiniStatuslineFileinfo",
-    --           strings = {fileinfo},
-    --         },
-    --         { hl = mode_hl, strings = { "%l:%v" } },
-    --       })
-    --     end,
-    --   },
-    -- })
 
     local miniclue = require('mini.clue')
     miniclue.setup({
