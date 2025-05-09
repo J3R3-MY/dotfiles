618097332f0bcab258a0a50bb91f8a56005680e6
J3R3-MY <urnmd@student.kit.edu>
2025-05-09 14:29:49 +0200

    Rework Copilot integration
    -Use Copilot as source for blink-cmp
    -Formatted blink.lua
    -Added blink icon for copilot suggestions

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,39 +1,52 @@
 return {
-    {
-        "saghen/blink.compat",
-        -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
-        version = "*",
-        -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
-        lazy = true,
-        -- make sure to set opts so that lazy.nvim calls blink.compat's setup
-        opts = {},
-    },
-    {
-        "saghen/blink.cmp",
-        dependencies = {
-            "rafamadriz/friendly-snippets",
-        },
+	{
+		"saghen/blink.compat",
+		-- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+		version = "*",
+		-- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+		lazy = true,
+		-- make sure to set opts so that lazy.nvim calls blink.compat's setup
+		opts = {},
+	},
+	{
+		"saghen/blink.cmp",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"giuxtaposition/blink-cmp-copilot"
+		},
 
-        -- use a release tag to download pre-built binaries
-        version = "1.*",
-        opts = {
-            keymap = {
-                preset = "enter",
-								["<C-j>"] = { "show", "select_next", "fallback" },
-								["<C-k>"] = { "show", "select_prev", "fallback" },
-            },
+		-- use a release tag to download pre-built binaries
+		version = "1.*",
+		opts = {
+			keymap = {
+				preset = "enter",
+				["<C-j>"] = { "show", "select_next", "fallback" },
+				["<C-k>"] = { "show", "select_prev", "fallback" },
+			},
 
-            appearance = {
-                nerd_font_variant = "mono",
-            },
+			completion = { documentation = { auto_show = true } },
+			signature = { enabled = true },
 
-            completion = { documentation = { auto_show = true } },
-            signature = { enabled = true },
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer", "copilot" },
+				providers = {
+					copilot = {
+						name = "copilot",
+						module = "blink-cmp-copilot",
+						score_offset = 100,
+						async = true,
+						transform_items = function(_, items)
+							for _, item in ipairs(items) do
+								item.kind_icon = ""
+								item.kind_name = "Copilot"
+							end
+							return items
+						end,
+					},
+				},
+			},
 
-            sources = {
-                default = { "lsp", "path", "snippets", "buffer"},
-            },
-        },
-        opts_extend = { "sources.default" },
-    },
+			opts_extend = { "sources.default" },
+		},
+	}
 }
