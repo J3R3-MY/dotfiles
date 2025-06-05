fa1f4f958ef9554be6f9eea6a5355b55062cb181
J3R3-MY <urnmd@kit.edu>
2025-06-05 23:56:26 +0200

    Add flash.nvim
    -Added flash for faster movement
    -Changed arrow.lua keymap to not clash with flash

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,12 @@
+return{
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+  },
+}
