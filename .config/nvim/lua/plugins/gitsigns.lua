60b798d76bc8f23dd9b3d0f0e2fc26ef36342273
Jeremi <urnmd@student.kit.edu>
2025-11-22 17:05:08 +0100

    feat(git): Improve git usage
    -Added diffview.nvim for mergetool and file history functionality
    -Changed global gitconfig to use diffview.nvim as mergetool

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -18,17 +18,7 @@ return {
 
       map({"n", "v"}, "<leader>hu", ":Gitsigns undo_stage_hunk<CR>", "Undo stage hunk")
 
-      map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
-
-      map("n", "<leader>hb", function()
-        gs.blame_line({ full = true })
-      end, "Blame line")
       map("n", "<leader>hB", gs.toggle_current_line_blame, "Toggle line blame")
-
-      map("n", "<leader>hd", gs.diffthis, "Diff this")
-      map("n", "<leader>hD", function()
-        gs.diffthis("~")
-      end, "Diff this ~")
 
       -- Text object
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "Gitsigns select hunk")
