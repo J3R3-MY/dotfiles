158410b78f8e4e8f0c3a9bb3dee44488bb6b861e
Jeremi Klinke <jklinke@cinemo.com>
2026-04-01 12:58:57 +0200

    cleanup(plugins): Remove uneeded plugins

--- .config/nvim/lua/plugins/scissors.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "chrisgrieser/nvim-scissors",
-  dependencies = "folke/snacks.nvim",
-  config = function()
-    require("scissors").setup({
-      snippetDir = vim.fn.stdpath("config") .. "/snippets/vscode",
-    })
-  end,
-}
